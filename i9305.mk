2ba54d5c752b0052aa08ff37328b0b432c1bdb66
rodman01 <email4joern@arcor.de>
2015-09-27 07:28:53 +0200

    fix bluetooth error
    Change-Id: Ia5c0f756eac1d0685b95ad8ac0440e0661e9b879

--- i9305.mk
+++ i9305.mk
@@ -37,11 +37,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/tiny_hw.xml:system/etc/sound/m3
     
 # Bluetooth    
-PRODUCT_COPY_FILES := \
+PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/bluetooth/libbt-vendor.so:system/vendor/lib/libbt-vendor.so
     
 # Boothack   
-PRODUCT_COPY_FILES := \
+PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/boothack/libandroid_runtime.so:system/lib/libandroid_runtime.so \
     $(LOCAL_PATH)/boothack/libandroid_servers.so:system/lib/libandroid_servers.so
 
